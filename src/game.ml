04812a9868f271196c0035dbb37b8d4c01a00b14
xforeverjlx <43357086+xforeverjlx@users.noreply.github.com>
2022-03-16 00:50:52 -0400

    changed queue_to_list to preserve order

--- src/game.ml
+++ src/game.ml
@@ -53,7 +53,7 @@ let list_to_queue players =
   helper players myqueue
 
 (** [queue_to_list players] converts the queue of players to a list *)
-let queue_to_list q = Queue.fold (fun x y -> y :: x) [] q
+let queue_to_list q = List.rev (Queue.fold (fun x y -> y :: x) [] q)
 
 (** [players_to_hands] returns a list containing the hands of each
     player in [p] *)
