d4738bf4d7d9babe878c009d50bf7b99d242a25d
Johann Lee <jcl354@cornell.edu>
2022-03-25 17:07:35 -0400

    finished easy_difficulty bot

--- src/bot.ml
+++ src/bot.ml
@@ -2,14 +2,15 @@ open Card
 
 type bot_level = Easy
 
-let easy_decision_rule
-    (threshold : int)
-    (strength : int)
+let decision_rule
+    (my_strength : int)
+    (opp_strength : int)
     (wealth : int)
     (min_raise : int) : Game.command =
-  if threshold < strength then
-    Raise (min_raise + ((strength - threshold) * (wealth / 20)))
-  else if threshold < strength + 2 then Call
+  let threshold = Random.int 3 in
+  if threshold <= my_strength - opp_strength then
+    Raise (min_raise + ((my_strength - opp_strength) * (wealth / 20)))
+  else if threshold < my_strength - opp_strength + 2 then Call
   else Fold
 
 (* (** [next_move_easy hand table wealth min_raise] returns the next
@@ -23,20 +24,37 @@ let easy_decision_rule
    ran_int (rank_of_hand (hand @ table)) wealth min_raise *)
 
 (** [run_rollouts deck num_rollouts cur_cards] simulates [num_rollouts]
-    number of rollouts and returns a list containing the hands under
-    those rollouts *)
+    number of rollouts till 7 cards and returns a list containing the
+    hands under those rollouts *)
 let rec run_rollouts
     (deck : Card.t)
     (num_rollouts : int)
-    (cur_cards : Card.t) : Card.t list =
+    (cur_cards : Card.t)
+    (num_cards_to_rollout : int) : Card.t list =
   match num_rollouts with
   | 0 -> []
   | _ ->
       let drawn_cards, _ =
-        Card.n_random_card deck (7 - List.length cur_cards)
+        Card.n_random_card deck num_cards_to_rollout
       in
       (drawn_cards @ cur_cards)
       :: run_rollouts deck (num_rollouts - 1) cur_cards
+           num_cards_to_rollout
+
+let list_sum lst = List.fold_left (fun x y -> x + y) 0 lst
+
+(** [avg_hand_strength deck num_rollouts hand] runs [num_rollouts]
+    number of simulations until completion (7 cards) then returns the
+    average hand strength of the 7 cards *)
+let rec avg_hand_strength deck num_rollouts hand acc =
+  match acc with
+  | 0 -> []
+  | _ ->
+      let rollouts = run_rollouts deck num_rollouts hand acc in
+      let avg =
+        list_sum (List.map rank_of_hand rollouts) / num_rollouts
+      in
+      avg :: avg_hand_strength deck num_rollouts hand (acc - 1)
 
 (** [next_move_easy hand table wealth min_raise] returns the next move
     (Call, Raise x, Fold) of the medium difficulty bot given cards
@@ -48,17 +66,24 @@ let next_move_easy
     (deck : Card.t)
     (wealth : int)
     (min_raise : int) : Game.command =
-  let ran_int = Random.int 10 in
-  let num_rollouts = 100 in
-  let rollout_hands = run_rollouts deck num_rollouts (hand @ table) in
-  let average_hand_strength =
-    List.fold_left
-      (fun x y -> x + y)
-      0
-      (List.map rank_of_hand rollout_hands)
-    / num_rollouts
-  in
-  easy_decision_rule ran_int average_hand_strength wealth min_raise
+  if List.length table = 0 then
+    decision_rule
+      (starting_hand_estimated_strength hand)
+      0 wealth min_raise
+  else
+    let num_rollouts = 100 in
+    let my_avg_strength =
+      (avg_hand_strength deck num_rollouts (hand @ table)
+         (5 - List.length table)
+      |> list_sum)
+      / (5 - List.length table)
+    in
+    let opp_avg_strength =
+      (avg_hand_strength deck num_rollouts table (7 - List.length table)
+      |> list_sum)
+      / (7 - List.length table)
+    in
+    decision_rule my_avg_strength opp_avg_strength wealth min_raise
 
 let next_move
     bot_level
