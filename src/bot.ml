b34323bbd9ae494116ed15c8b5ae4b3378dcd804
Johann Lee <jcl354@cornell.edu>
2022-03-25 15:17:19 -0400

    finished easy bot

--- src/bot.ml
+++ src/bot.ml
@@ -1,1 +1,25 @@
-let next_move = failwith "unimplmented"
+open Card
+
+let easy_decision_rule
+    (threshold : int)
+    (strength : int)
+    (wealth : int)
+    (min_raise : int) : Game.command =
+  if threshold < strength then
+    Raise (min_raise + ((strength - threshold) * (wealth / 20)))
+  else if threshold < strength + 2 then Call
+  else Fold
+
+let next_move_easy
+    (hand : Card.t)
+    (table : Card.t)
+    (wealth : int)
+    (min_raise : int) : Game.command =
+  let ran_int = Random.int 10 in
+  if List.length (hand @ table) = 2 then
+    easy_decision_rule ran_int wealth min_raise
+      (2 * starting_hand_estimated_strength hand)
+  else
+    easy_decision_rule ran_int
+      (rank_of_hand (hand @ table))
+      wealth min_raise
--- src/bot.mli
+++ src/bot.mli
@@ -1,1 +1,4 @@
-val next_move : Card.t -> Card.t -> Game.command
+val next_move_easy : Card.t -> Card.t -> int -> int -> Game.command
+(** [next_move_easy hand table wealth min_raise] returns the next move
+    (Call, Raise x, Fold) of the easy difficulty bot given cards [hand]
+    and [table] and states [wealth] and [min_raise]*)
